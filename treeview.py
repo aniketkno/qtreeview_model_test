0663fbbd3e7d3fe790976cc4136361b6a07d8101
aniketkno <3714725+aniketkno@users.noreply.github.com>
2021-02-06 08:31:13 -0600

    changes to Treeview.py
    changed to a qwidget, getting values from rows
    
    This was a really helpful exercise!

--- treeview.py
+++ treeview.py
@@ -1,81 +1,92 @@
-from PyQt4 import QtCore, QtGui, uic
 import sys
-sys.path.append("C:/Python27/Lib/site-packages")
+sys.path.append('C:/Python38/Lib/site-packages')
+from PyQt5.QtWidgets import QWidget,QApplication, QMainWindow, QTreeView, QCheckBox, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.Qt import QStandardItemModel, QStandardItem
+from PyQt5.QtGui import QFont, QColor
+from PyQt5.QtCore import Qt
 
 
-class testTreeview(QtGui.QDialog):
-    def __init__(self, data={}, parent=None):
+class StandardItem(QStandardItem):
+    def __init__(self, txt='', font_size =12, set_bold = False, color=QColor(0,0,0)):
+        super().__init__()
+        fnt = QFont('Open Sans', font_size)
+        fnt.setBold(set_bold)
+        self.setEditable(False)
+        self.setForeground(color)
+        self.setFont(fnt)
+        self.setText(txt)
+
+class testTreeview(QWidget):
+    def __init__(self, parent=None):
         super(testTreeview, self).__init__(parent)
-        uic.loadUi("TreeviewTest.ui", self)
-        self.CB = 0
-        self.VIEWPORT = 1
-        self.CAMERA = 2
-        self.MOV_TYPE = 3
-        self.MOV_NAME = 4
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+        self.setWindowTitle("World Diagram")
+        # self.resize(500, 700)
 
-        self.data = data
-        self.mov_type = ['', 'WIREFRAME', 'SHADED', 'PERSP', 'CONE', 'CUSTOM']
+        treeView = QTreeView()
+        vbox.addWidget(treeView)
+        vbox.addLayout(hbox)
+        self.ok_Btn = QPushButton('OK')
+        self.cancel_Btn = QPushButton('Cancel')
+        # hbox.addStretch(1)
+        hbox.addWidget(self.ok_Btn)
+        hbox.addWidget(self.cancel_Btn)
+        # vbox.addStretch(1)
+        # row types
+        self.CB = 0
+        self.ONE = 1
+        self.TWO = 2
+        self.THREE = 3
+
+        self.treeModel = QStandardItemModel()
         self.model = self.create_data_model(self)
-        print(self.data)
-        self.treeView.setModel(self.model)
-        self.treeView.header().setVisible(False)
-        self.treeView.setEditTriggers(self.treeView.NoEditTriggers)
-        self.treeView.setSelectionMode(self.treeView.ExtendedSelection)
-        # self.btn_cancel.clicked.connect(self.close)
-        for row, viewport, mov_name in zip(range(len(self.data.keys())), self.data.keys(), self.data.values()):
-            print(row, viewport, mov_name)
-            self.addData_to_model(row, viewport, mov_name)
+        self.rootNode = self.model.invisibleRootItem()
+
+        for row in range(3):
+            print(row)
+            self.rootNode.appendRow(self.addData_to_model(row))
             print()
 
+        treeView.setModel(self.model)
+        # self.setCentralWidget(treeView)
+  
+  
+        # setting geometry of button 
+        # adding action to a button 
+        self.ok_Btn.clicked.connect(self.clickme) 
+        self.cancel_Btn.clicked.connect(self.close)
+
     def create_data_model(self, parent):
-        model = QtGui.QStandardItemModel(0, 5, parent)
-        model.setHeaderData(self.CB, QtCore.Qt.Horizontal, "")
-        model.setHeaderData(self.VIEWPORT, QtCore.Qt.Horizontal, "Viewport")
-        model.setHeaderData(self.CAMERA, QtCore.Qt.Horizontal, "Camera")
-        model.setHeaderData(self.MOV_TYPE, QtCore.Qt.Horizontal, "Mov Type")
-        model.setHeaderData(self.MOV_NAME, QtCore.Qt.Horizontal, "Mov Name")
+        model = QStandardItemModel(0, 4, parent)
+        model.setHeaderData(self.CB, Qt.Horizontal, "1")
+        model.setHeaderData(self.ONE, Qt.Horizontal, "2")
+        model.setHeaderData(self.TWO, Qt.Horizontal, "3")
+        model.setHeaderData(self.THREE, Qt.Horizontal, "4")
         return model
 
-    def addData_to_model(self, row, viewport, mov_name ):
-        self.model.setData(self.model.index(row, self.VIEWPORT), viewport)
-        self.model.setData(self.model.index(row, self.CAMERA), self.get_cam_data(viewport))
-        self.model.setData(self.model.index(row, self.MOV_TYPE), self.get_mov_type(mov_name))
-        self.model.setData(self.model.index(row, self.MOV_TYPE), self.set_mov_type(mov_name))
+    def addData_to_model(self, row):
+        cb = StandardItem('')
+        cb.setCheckable(True)
+        one = StandardItem('1')
+        two = StandardItem('2')
+        three = StandardItem('3')
+        return [cb, one, two, three]
 
-        
+    def clickme(self):
+        print(self.rootNode.rowCount())
+        dict_data = {}
+        for row in range(self.rootNode.rowCount()):
+            print("{0} checkBox".format(row), self.rootNode.child(row, 0).checkState())
+            if self.rootNode.child(row, 0).checkState():
+                tm1 = self.rootNode.child(row,1)
+                tm2 = self.rootNode.child(row,2)
+                tm3 = self.rootNode.child(row,3)
+                dict_data.setdefault(row, [tm1.data(0), tm2.data(0), tm3.data(0)])
+        print(dict_data)
 
-    def get_cam_data(self, viewport = ''):
-        cam_data = {"modelPanel1": "RODCam1",
-                    "modelPanel2": "RODCam1",
-                    "modelPanel3": "Persp"
-                    }
-        print("camera: ", cam_data[viewport])
-        return cam_data[viewport]
-
-    def get_mov_type(self, mov_name):
-        if mov_name in 'skip':
-            print("movie type: ", self.mov_type[[index for index, string in enumerate(self.mov_type) if '' in string][0]])
-            return self.mov_type[[index for index, string in enumerate(self.mov_type) if '' in string][0]]
-        elif mov_name not in self.mov_type:
-            print("movie type: ", self.mov_type[[index for index, string in enumerate(self.mov_type) if 'CUSTOM' in string][0]])
-            return self.mov_type[[index for index, string in enumerate(self.mov_type) if 'CUSTOM' in string][0]]
-        else:
-            print("movie type: ", self.mov_type[[index for index, string in enumerate(self.mov_type) if mov_name in string][0]])
-            return self.mov_type[[index for index, string in enumerate(self.mov_type) if mov_name in string][0]]
-
-            
-    def set_mov_type(self, mov_name):
-        if mov_name == 'skip':
-            return ''
-        else:
-            return mov_name
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    data = {"modelPanel1": "CONE",
-            "modelPanel2": "skip",
-            "modelPanel3": "TEST"
-            }
-    win = testTreeview(data)
-    win.show()
-    app.exec_()
+app = QApplication(sys.argv)
+win = testTreeview()
+win.show()
+sys.exit(app.exec_())
